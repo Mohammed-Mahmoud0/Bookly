10ddeee29e0d8fc5c3b70cd7a4b726c985b53ed1
Mohamed Mahmoud <mf.mohamedfawzy0@gmail.com>
2025-06-10 19:53:48 +0300

    add custom app bar

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,10 +1,12 @@
+import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(body: Column(children: [CustomAppBar()]));
   }
 }
