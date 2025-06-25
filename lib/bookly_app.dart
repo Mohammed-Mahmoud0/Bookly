a15d1e3bda5153170c9306985104130448466b70
Mohamed Mahmoud <mf.mohamedfawzy0@gmail.com>
2025-06-25 15:02:49 +0300

    remove get and use go_router for app routing

--- lib/bookly_app.dart
+++ lib/bookly_app.dart
@@ -1,7 +1,6 @@
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/constants.dart';
-import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/get_navigation.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class BooklyApp extends StatelessWidget {
@@ -9,14 +8,16 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp.router(
+      routerConfig: AppRouter.router,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: kPrimaryColor,
         brightness: Brightness.dark,
         textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
       ),
-      home: const SplashView(),
     );
   }
 }
+
+
