3847427967a4cbf139a218dee0ce213d03c1c846
Mohamed Mahmoud <mf.mohamedfawzy0@gmail.com>
2025-09-07 18:27:01 +0300

    newest books cubit added

---
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var data = await homeRepo.fetchNewestBooks();
+    data.fold(
+      (failure) {
+        emit(NewestBooksFailure(failure.errorMessage));
+      },
+      (newestBooks) {
+        emit(NewestBooksSuccess(newestBooks));
+      },
+    );
+  }
+}
