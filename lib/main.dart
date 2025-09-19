f64f9cf255ad9791fd6d79bf0840b5e50dbec663
Mohamed Mahmoud <mf.mohamedfawzy0@gmail.com>
2025-09-19 11:00:59 +0300

    setup service locator (dependency injection & singelton app services )

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:bookly/bookly_app.dart';
+import 'package:bookly/core/utils/service_locator.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const BooklyApp());
 }
